4fc236348a197df707b36e6cafc50636734e6a2f
huishu17 <128003755+huishu17@users.noreply.github.com>
2023-03-16 11:54:36 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,31 @@
+input.onPinPressed(TouchPin.P0, function () {
+    if (y > 0) {
+        led.unplot(x, y)
+        y += -1
+    }
+})
 input.onButtonPressed(Button.A, function () {
     if (x > 0) {
-        led.unplot(x, 0)
+        led.unplot(x, y)
         x += -1
     }
 })
 input.onButtonPressed(Button.B, function () {
     if (x < 4) {
-        led.unplot(x, 0)
+        led.unplot(x, y)
         x += 1
     }
 })
+input.onPinPressed(TouchPin.P1, function () {
+    if (y < 4) {
+        led.unplot(x, y)
+        y += 1
+    }
+})
+let y = 0
 let x = 0
 x = 2
+y = 2
 basic.forever(function () {
-    led.plot(x, 0)
+    led.plot(x, y)
 })
