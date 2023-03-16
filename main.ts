bc8620769c974f68a54fd5bd7315c97eb3c0ac56
huishu17 <128003755+huishu17@users.noreply.github.com>
2023-03-16 08:27:59 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,14 @@
 input.onButtonPressed(Button.A, function () {
-    led.unplot(x, 0)
-    x += -1
+    if (x > 0) {
+        led.unplot(x, 0)
+        x += -1
+    }
 })
 input.onButtonPressed(Button.B, function () {
-    led.unplot(x, 0)
-    x += 1
+    if (x < 4) {
+        led.unplot(x, 0)
+        x += 1
+    }
 })
 let x = 0
 x = 2
